66db7362d846ce86736ee8e31709f0649b243093
aashir-tech <aashirharis6@gmail.com>
2025-08-21 14:57:52 +0530

    Removed unused module

--- app/admin/dashboard/home-management/page.tsx
+++
@@ -1,5 +0,0 @@
-import HomePageManager from '@/components/admin/HomePageManager';
-
-export default function HomeManagementPage() {
-  return <HomePageManager />;
-}
--- app/admin/dashboard/navbar-manager/page.tsx
+++
@@ -1,5 +0,0 @@
-// import NavbarManager from '@/components/admin/NavbarManager';
-
-// export default function NavbarManagementPage() {
-//   return <NavbarManager />;
-// }
