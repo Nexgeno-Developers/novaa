b087b06548de33046bb983dce0142d980ed6d8cf
aashir-tech <aashirharis6@gmail.com>
2025-09-24 00:32:35 +0530

    Fix CLient wrapper to have a redux store provider

--- components/admin/ClientWrapper.tsx
+++ components/admin/ClientWrapper.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { Provider } from "react-redux";
+import { store } from "@/redux";
+
 interface ClientWrapperProps {
   children: React.ReactNode;
 }
 
 export default function ClientWrapper({ children }: ClientWrapperProps) {
-  return <>{children}</>;
+  return <Provider store={store}>{children}</Provider>;
 }
